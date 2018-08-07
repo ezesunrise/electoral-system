0c8f91b93dfd645515c68e98d107bd6998fe1ce7
Sunrise Eze <ezesunrise@yahoo.com>
2018-08-07 22:58:38 +0100

    Add project files.

---
+++ ElectoralSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElectoralSystem.Startup))]
+namespace ElectoralSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
